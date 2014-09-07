e2febef32d466387c0144defe8b4e74608adab3d
usei <useiichi@hotmail.com>
2014-09-07 15:12:23 +0900

    fds

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,7 +10,15 @@ Taka::Application.configure do
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  #config.eager_load = false
+  config.eager_load = true
+
+  # Show full error reports and disable caching.
+  #config.consider_all_requests_local       = true
+  #config.action_controller.perform_caching = false
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
 
   # Configure static asset server for tests with Cache-Control for performance.
   config.serve_static_assets  = false
@@ -30,10 +38,6 @@ Taka::Application.configure do
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
-
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
