32cd5563615946db3087f06b4f831c7279096249
usei <useiichi@hotmail.com>
2014-09-07 15:08:21 +0900

    fds

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,8 +13,23 @@ Taka::Application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_assets  = true
-  config.static_cache_control = "public, max-age=3600"
+  config.serve_static_assets  = false
+  #config.static_cache_control = "public, max-age=3600"
+
+
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
+  # Generate digests for assets URLs.
+  config.assets.digest = true
+
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
+
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
