7174b002608a0b9613ac149e829734266882b4ef
Your Name <you@example.com>
2015-05-03 05:43:50 +0000

    fds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root :to => "messages#index"
+#  get 'sessions/new'
+#
+#  get 'sessions/create'
+#
+#  get 'sessions/destroy'
+
+  resources :sessions
   resources :messages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
