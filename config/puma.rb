d1a87e1101040f336532b1de1c60efb47de4edf6
CoreOS Admin <core@node1.c.root-talon-161808.internal>
2017-05-01 16:35:28 +0900

    fds

--- config/puma.rb
+++ config/puma.rb
@@ -76,7 +76,7 @@ end
 # The default is "tcp://0.0.0.0:9292".
 #
 # bind 'tcp://0.0.0.0:9292'
-# bind 'unix:///var/run/puma.sock'
+bind 'unix:///var/run/puma-taka.sock'
 # bind 'unix:///var/run/puma.sock?umask=0111'
 # bind 'ssl://127.0.0.1:9292?key=path_to_key&cert=path_to_cert'
 
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,8 +12,8 @@
 
 ActiveRecord::Schema.define(version: 20150503043611) do
 
-  create_table "messages", force: :cascade do |t|
-    t.integer  "userid",     limit: 4
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "userid"
     t.text     "body",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
