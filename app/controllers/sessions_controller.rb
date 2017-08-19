6d626ef48c863d41a5fd2e4d1abc00bda1b30c6b
CoreOS Admin <core@node1.c.root-talon-161808.internal>
2017-08-19 18:04:20 +0900

    fds

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:password] == "sakura"
+      session[:user_id] = 1
+      redirect_to :controller => 'messages', :action => 'index'
+    elsif params[:password] == "umesei"
+      session[:user_id] = 2
+      redirect_to :controller => 'messages', :action => 'index'
+    elsif params[:password] == "aaa"
+      session[:user_id] = 3
+      redirect_to :controller => 'messages', :action => 'index'
+    elsif params[:password] == "uuuaaa"
+      session[:user_id] = 4
+      redirect_to :controller => 'messages', :action => 'index'
+    else
+      #session[:user_id] = nil
+      session.delete(:user_id)
+      flash.now.alert = "Invalid email or password"
+      render "new"
+    end
+  end
+
+  def destroy
+      #session[:user_id] = nil
+      session.delete(:user_id)
+      #redirect_to :controller => 'messages', :action => 'index'
+      redirect_to root_path
+  end
+end
